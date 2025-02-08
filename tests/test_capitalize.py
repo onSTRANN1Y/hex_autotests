4f8ec3275c776132ef6e7f05b270137a902b55da
onSTRANN1y <splashilia3@gmail.com>
2025-02-08 21:35:55 +0300

    first commit

---
+++ tests/test_capitalize.py
@@ -0,0 +1,10 @@
+from code.capitalize import capitalize
+
+print('hi')
+
+if capitalize('hello!') != 'Hello!':
+    raise Exception(f'Ошибка:Ожидается вывод "Hello", вывелось {capitalize('hello')}')
+if capitalize('') != '':
+    raise Exception('Ошибка')
+print('Все тесты выполнены. Ошибок нет!')
+
